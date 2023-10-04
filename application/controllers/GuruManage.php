3416734f50814654544a6ab9dc6e30072cdc6ec7
KepoDes <86428587+Kepoo311@users.noreply.github.com>
2023-10-04 17:24:10 +0700

    Add files via upload

--- application/controllers/GuruManage.php
+++ application/controllers/GuruManage.php
@@ -9,8 +9,9 @@ class GuruManage extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('Absensi_model');
-        $this->load->model('mabsensi');
+    $this->load->model('minputdata');
+		$this->load->model('mabsensi');
+		$this->load->model('mlaporan');
     	$this->load->helper('url');
 		$this->load->library('session');
 	}
@@ -25,6 +26,7 @@ class GuruManage extends CI_Controller {
 		$data['guru'] = $this->mabsensi->jumlah_guru();
 		$data['kelas'] = $this->mabsensi->jumlah_kelas();
 		$data['sudah_absen'] = $this->mabsensi->j_kelas_sudah_absen(TODAY_DATE);
+    $data['guru_sudah_absen'] = $this->mabsensi->j_guru_sudah_absen(TODAY_DATE);
 		$data['jadwal'] = $this->mabsensi->jumlah_jadwal();
 		$data['guruAlpa'] = $this->mabsensi->GuruAlpa(TODAY_DATE);
 
@@ -76,6 +78,25 @@ class GuruManage extends CI_Controller {
         $this->load->view('guru_manage/layouts/navbar');
         $this->load->view('guru_manage/layouts/sidebar');
         $this->load->view('guru_manage/laporan/bulan');
+        $this->load->view('guru_manage/layouts/footer');
+        $this->load->view('guru_manage/laporan/script');
+    }
+    public function laporan_hari()
+    {
+        if ($this->session->userdata('role') != 'Guru Manage') {
+          redirect('','refresh');
+        }
+
+        $data['guru_sudah_absen'] = $this->mabsensi->jumlah_guru() - $this->mabsensi->j_guru_sudah_absen(TODAY_DATE);
+
+        $list_guru_sudah_absen = $this->mlaporan->guru_sudah_absen(TODAY_DATE);
+
+        $data['nama_guru'] = $list_guru_sudah_absen;
+
+        $this->load->view('guru_manage/layouts/meta');
+        $this->load->view('guru_manage/layouts/navbar');
+        $this->load->view('guru_manage/layouts/sidebar');
+        $this->load->view('guru_manage/laporan/hari', $data);
         $this->load->view('guru_manage/layouts/footer');
         $this->load->view('guru_manage/laporan/script');
     }
